3d94f2a1d516f535af1398d953ef52fc38372d67
alaqqad <alialaqqad12@gmail.com>
2022-08-18 19:18:59 +0300

    Edit the return in playerChoice function in script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function getComputerChoice () {
-    //comp random
-    const choicesArray = ["ROCK", "PAPER", "SCISSORS"];
-    const randomChoices = Math.floor(Math.random() * choicesArray.length);
-    console.log(choicesArray[randomChoices]);
-    return choicesArray[randomChoices];
-}
-
 function playerChoice () {
     let input = prompt("Type rock, paper, or scissors");
     while (input == null) {
         input = prompt("Type rock, paper, or scissors");
     }
     input = input.toUpperCase();
+    console.log(`Player choice ${input}`);
     return input;
+}
+
+function getComputerChoice () {
+    //comp random
+    const choicesArray = ["ROCK", "PAPER", "SCISSORS"];
+    const randomChoices = Math.floor(Math.random() * choicesArray.length);
+    console.log(`Computer choice ${choicesArray[randomChoices]}`);
+    return choicesArray[randomChoices];
 }
 
 function checkWinner (choiceP, choiceC) {
