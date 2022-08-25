cdf0de45f2a11dd5ba51a38655b74df67b4745b6
alaqqad <alialaqqad12@gmail.com>
2022-08-25 19:50:30 +0300

    Edit add results section at the end and add round number on script.js

--- script.js
+++ script.js
@@ -1,43 +1,66 @@
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(i);
-    }
-}
+//global variables
+const winners = [];
+const choicesArray = ["rock", "paper", "scissors"];
 
+//player input
 function playerChoice () {
     let input = prompt("Type rock, paper, or scissors");
     while (input == null) {
         input = prompt("Type rock, paper, or scissors");
     }
-    input = input.toUpperCase();
+    input = input.toLowerCase();
     console.log(`Player choice: ${input}`);
     return input;
 }
 
+//computer input random
 function getComputerChoice () {
-    //comp random
-    const choicesArray = ["ROCK", "PAPER", "SCISSORS"];
     const randomChoices = Math.floor(Math.random() * choicesArray.length);
     console.log(`Computer choice: ${choicesArray[randomChoices]}`);
     return choicesArray[randomChoices];
 }
 
+//connect computer random input with user input
 function checkWinner (choiceP, choiceC) {
     if (choiceP === choiceC) {
-        return "Result: Tie";
-    } else if ((choiceP === "ROCK" && choiceC == "SCISSORS") || (choiceP === "PAPER" && choiceC == "ROCK") || (choiceP === "SCISSORS" && choiceC == "PAPER")) {
-        return "Result: Player Won";
+        return "Tie, no win no lose";
+    } else if ((choiceP === "rock" && choiceC == "scissors") || 
+    (choiceP === "paper" && choiceC == "rock") || 
+    (choiceP === "scissors" && choiceC == "paper")) {
+        return "You won this round";
     } else {
-        return "Result: Computer Won";
+        return "Computer won this round";
     }
     }
 
+//results
+function logWin () {
+     let playerWins = winners.filter((winner) => winner == "You won this round").length;
+     let compWins = winners.filter((winner) => winner == "Computer won this round").length;
+     let tie = winners.filter((winner) => winner == "Tie, no win no lose").length;
+     console.log('Results:');
+     console.log('Player wins: ', playerWins);
+     console.log('Computer wins: ', compWins);
+     console.log('Ties', tie);
+}
+
 function playRound (round) {
+    console.log('Round: ', round);
     const playerSelection = playerChoice();
     const computerSelection = getComputerChoice();
     const winner = checkWinner(playerSelection, computerSelection);
     console.log(winner);
+    winners.push(winner);
+    console.log("-----------------------------------------------");
 }
 
+// 5 game rounds
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+      playRound(i);
+    }
+    return logWin();
+}
 
 game();
